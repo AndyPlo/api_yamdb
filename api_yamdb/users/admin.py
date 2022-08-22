a2f0a42b048e497977d0c76eb74a26a4d1c3c67a
Andrei Plotnikov <avin@mail.ru>
2022-08-23 00:31:29 +0500

    user model changed

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'username', 'email', 'role', 'bio', 'first_name', 'last_name',
+    )
+    list_editable = ('role',)
+    search_fields = ('username', 'role',)
+    empty_value_display = '-пусто-'
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+ROLES = [
+    ('user', 'user'),
+    ('admin', 'admin'),
+    ('moderator', 'moderator')
+]
+
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        blank=True
+    )
+    role = models.SlugField(
+        'Роль',
+        choices=ROLES,
+        default='user'
+    )
