c640243c2183c2bcf3a0d3c84fc015341149ec12
Andrei Plotnikov <avin@mail.ru>
2022-08-28 13:44:27 +0500

    permissions done

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,60 +1,59 @@
 from rest_framework import permissions
 
 
-# class ReadOnly(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         return request.method in permissions.SAFE_METHODS
-
-
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        # if request.user.is_anonymous:
-        #     return False
-        # user = request.user
-        return (request.user.role == 'admin'
-                or request.user.is_superuser)
+        if request.user.is_anonymous:
+            return False
+        else:
+            return (request.user.role == 'admin'
+                    or request.user.is_superuser)
 
     def has_object_permission(self, request, view, obj):
-        # user = request.user
-        return (request.user.role == 'admin'
-                or request.user.is_superuser)
+        if request.user.is_anonymous:
+            return False
+        else:
+            return (request.user.role == 'admin'
+                    or request.user.is_superuser)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_anonymous and request.method in permissions.SAFE_METHODS:
-            return True
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.role == 'admin'
-            or request.user.is_superuser
-        )
+        if request.user.is_anonymous:
+            return request.method in permissions.SAFE_METHODS
+        else:
+            return (
+                request.method in permissions.SAFE_METHODS
+                or request.user.role == 'admin'
+                or request.user.is_superuser
+            )
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_anonymous and request.method in permissions.SAFE_METHODS:
-            return True
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.role == 'admin'
-            or request.user.is_superuser
-        )
+        if request.user.is_anonymous:
+            return request.method in permissions.SAFE_METHODS
+        else:
+            return (
+                request.method in permissions.SAFE_METHODS
+                or request.user.role == 'admin'
+                or request.user.is_superuser
+            )
 
 
 class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            # or request.user.is_authenticated
-            # or request.user.role == 'moderator'
-            # or request.user.role == 'admin'
-            # or request.user.is_superuser
-        )
+        if request.user.is_anonymous:
+            return request.method in permissions.SAFE_METHODS
+        else:
+            return True
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-            or request.user.role == 'moderator'
-            or request.user.role == 'admin'
-            or request.user.is_superuser
-        )
+        if request.user.is_anonymous:
+            return request.method in permissions.SAFE_METHODS
+        else:
+            return (
+                request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+                or request.user.role == 'moderator'
+                or request.user.role == 'admin'
+                or request.user.is_superuser
+            )
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -115,7 +115,7 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -129,4 +129,3 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
-
